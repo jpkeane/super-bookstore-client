ba922803bed7a9489a29082a97510c9da613f858
James Keane <jpkeane@me.com>
2016-01-15 18:47:52 +0000

    Removed displayColour from rental-state

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+
   actions: {
     imageShow() {
       this.set('isImageShowing', true);
--- app/models/rental-state.js
+++ app/models/rental-state.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  displayColour: DS.attr('string'),
   rentals: DS.hasMany('rental', {async: true})
 });
