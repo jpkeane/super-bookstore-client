aa87d273eda9c2ee3b1f4b7c1ef0968042cbbc6e
James Keane <jpkeane@me.com>
2016-01-15 15:34:43 +0000

    Added environment configuration

--- app/routes/landlord.js
+++ app/routes/landlord.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-	    return this.store.findAll('landlord');
-	  },
+    return this.store.findAll('landlord');
+  },
 });
