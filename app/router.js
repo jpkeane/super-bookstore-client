aa40ac983b59e53a97afc26ca273e8a33047697a
James Keane <jpkeane@me.com>
2016-01-15 22:50:09 +0000

    Ember nested routes

--- app/router.js
+++ app/router.js
@@ -8,8 +8,14 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('landlord');
-  this.route('rental');
+  this.route('landlords', {}, function () {
+    this.route('index', {path: ''});
+    this.route('show', {path: '/:landlord_id'});
+  });
+  this.route('rentals', function () {
+    this.route('index', {path: ''});
+    this.route('show', {path: '/:rental_id'});
+  });
 });
 
 export default Router;
